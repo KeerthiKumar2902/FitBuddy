9868c84373ca69c6a0702c94f1c7b8ebb72d6523
KeerthiKumar2902 <keerthikumarkk1@gmail.com>
2025-11-02 19:16:23 +0530

    Minor changes

--- src/firebase.js
+++ src/firebase.js
@@ -19,3 +19,4 @@ const app = initializeApp(firebaseConfig);
 // Initialize and export Firebase services
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+export const storage = getStorage(app);
