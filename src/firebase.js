9c85b7481014b707a9ad88cb1a534f14c284dbf2
KeerthiKumar2902 <keerthikumarkk1@gmail.com>
2025-11-02 19:17:18 +0530

    Minor changes

--- src/firebase.js
+++ src/firebase.js
@@ -2,6 +2,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration, using environment variables
 const firebaseConfig = {
